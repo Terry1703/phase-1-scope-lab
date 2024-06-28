be559fbc0f84094e343d333f80615c2fbe1c9236
Terry <terry.otieno@student.moringaschool.com>
2024-06-28 11:01:03 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+const leastFavoriteCustomer  = 'someone';
+
+function changeLeastFavoriteCustomer() {
+    
+    leastFavoriteCustomer = 'someone else'; 
+}
+
